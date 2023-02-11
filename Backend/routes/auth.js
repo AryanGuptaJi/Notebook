04365f2f2b45f1865abf34413bd17bdfeca3a077
Aryan Gupta <aryang23936@gmail.com>
2023-02-11 20:08:21 +0530

    Creating a middleware to decode user from JWT token

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser');
 
 const JWT_SECRET = 'Aryanisagoodb$oy';
 
-// Create a User using: POST "/api/auth/createuser". No login required.
+// Route 1: Create a User using: POST "/api/auth/createuser". No login required.
 router.post('/createuser', [
   body('name', 'Enter a valid name').isLength({ min: 3 }),
   body('email', 'Enter a valid email').isEmail(),
@@ -45,7 +46,7 @@ router.post('/createuser', [
     const authtoken = jwt.sign(data, JWT_SECRET);
     
     // res.json(user);
-    res.json({authtoken});
+    res.json({ authtoken });
     
   } 
   catch (error) {
@@ -54,7 +55,7 @@ router.post('/createuser', [
   }
 })
 
-// Authenticate a User using: POST "/api/auth/login". No login required
+// Route 2: Authenticate a User using: POST "/api/auth/login". No login required
 router.post('/login', [ 
   body('email', 'Enter a valid email').isEmail(), 
   body('password', 'Password cannot be blank').exists(), 
@@ -66,10 +67,10 @@ router.post('/login', [
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const {email, password} = req.body;
+  const { email, password } = req.body;
 
   try {
-    let user = await User.findOne({email});
+    let user = await User.findOne({ email });
     if(!user){
       return res.status(400).json({ error: "Invalid login attempt"});
     }
@@ -90,6 +91,19 @@ router.post('/login', [
 
   } 
   catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
+
+// Route 3: Get loggedin User Details using: POST "/api/auth/getuser". Login required
+router.post('/getuser', fetchuser,  async (req, res) => {
+
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password")
+    res.send(user)
+  } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Server Error");
   }
