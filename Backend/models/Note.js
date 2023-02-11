10eac1825d4c6f3df7d0fa8b32eb48cd48a3d31c
Aryan Gupta <aryang23936@gmail.com>
2023-02-11 22:43:39 +0530

    Fetching all notes & add a new note

--- Backend/models/Note.js
+++ Backend/models/Note.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title:{
         type: String,
         required: true
     },
-    desciption:{
-        type: String,
-        required: true, 
-         unique: true
+    description:{
+        type: String, 
+        
     },
     tag:{
         type: String,
@@ -20,4 +24,4 @@ const NotesSchema = new Schema({
     },
   });
 
-module.exports = mongoose.model('notes', NotesSchema);
+module.exports = mongoose.model('notes', NotesSchema) ;
