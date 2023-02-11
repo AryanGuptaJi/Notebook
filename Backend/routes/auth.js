001240c506a0ea26ca279ebec12c230e340a494f
Aryan Gupta <aryang23936@gmail.com>
2023-02-11 17:17:47 +0530

    Creating login endpoint & sending authtoken

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -7,7 +7,7 @@ var jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'Aryanisagoodb$oy';
 
-// Create a User using: POST "/api/auth/createuser". No login required
+// Create a User using: POST "/api/auth/createuser". No login required.
 router.post('/createuser', [
   body('name', 'Enter a valid name').isLength({ min: 3 }),
   body('email', 'Enter a valid email').isEmail(),
@@ -50,7 +50,48 @@ router.post('/createuser', [
   } 
   catch (error) {
     console.error(error.message);
-    res.status(500).send("Some Error occured");
+    res.status(500).send("Internal Server Error");
+  }
+})
+
+// Authenticate a User using: POST "/api/auth/login". No login required
+router.post('/login', [ 
+  body('email', 'Enter a valid email').isEmail(), 
+  body('password', 'Password cannot be blank').exists(), 
+], async (req, res) => {
+
+  // If there are errors, return Bad request and the errors.
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {email, password} = req.body;
+
+  try {
+    let user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({ error: "Invalid login attempt"});
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if(!passwordCompare){
+      return res.status(400).json({ error: "Invalid login attempt"});
+    }
+
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({authtoken});
+
+  } 
+  catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
   }
 })
 
